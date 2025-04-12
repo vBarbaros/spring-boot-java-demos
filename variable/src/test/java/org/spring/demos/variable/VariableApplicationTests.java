70f8ba0f5ece8cd5d8d036d1b48b8c7cad6dcbdd
vBarbaros <victor.barbarosh@gmail.com>
2025-04-12 15:02:23 -0400

    Defines variables package

---
+++ variable/src/test/java/org/spring/demos/variable/VariableApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.demos.variable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
